ffc441fc7442b60d8580030fced59a604e494bac
yeonghoonPark <huklmnj2@naver.com>
2023-10-22 22:31:01 +0900

    [feature] create CloseIcon.tsx and HamburgerIcon.tsx

---
+++ src/app/components/icon/HamburgerIcon.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { AiOutlineMenu } from "react-icons/ai";
+
+type Props = {
+    onClick: () => void;
+};
+
+export default function HamburgerIcon({ onClick }: Props) {
+    return <AiOutlineMenu className="text-xl cursor-pointer" onClick={onClick} />;
+}
