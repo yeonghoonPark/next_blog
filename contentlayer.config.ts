6ceeacc442490da59364e1bc5735f80aaab8342c
yeonghoonPark <huklmnj2@naver.com>
2023-11-04 17:27:11 +0900

    [feature] set the field of the post and set the source through rehype

---
+++ contentlayer.config.ts
@@ -0,0 +1,60 @@
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
+import highlight from "rehype-highlight";
+import rehypePrettyCode from "rehype-pretty-code";
+
+export const Post = defineDocumentType(() => ({
+    name: "Post",
+    contentType: "mdx",
+    filePathPattern: "**/*.mdx",
+
+    // mdx로 작성한 글 정보에 대해 입력해야하는 필드 정의
+    /*
+        [필드명]: {
+            type: '자료형',
+            required: '필수여부',
+        }
+    */
+
+    fields: {
+        title: {
+            type: "string",
+            required: true,
+        },
+        description: {
+            type: "string",
+            required: true,
+        },
+        category: {
+            type: "string",
+            required: true,
+        },
+        thumbnail: {
+            type: "string",
+            required: false,
+        },
+        createdAt: {
+            type: "date",
+            required: true,
+        },
+    },
+}));
+
+const contentSource = makeSource({
+    // mdx 파일이 저장 되어 있는 루트 폴더
+    contentDirPath: "data/posts",
+    documentTypes: [Post],
+    mdx: {
+        remarkPlugins: [],
+        rehypePlugins: [
+            [
+                rehypePrettyCode,
+                {
+                    theme: "dark-plus",
+                },
+                highlight,
+            ],
+        ],
+    },
+});
+
+export default contentSource;
