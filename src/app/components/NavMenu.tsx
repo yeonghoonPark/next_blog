8dd9bcc17aa50f71ef90780db846a95a9450dddf
yeonghoonPark <huklmnj2@naver.com>
2023-11-11 18:41:31 +0900

    [fix] problems with not showing the current path when a post in a header is dynamic routing

--- src/app/components/NavMenu.tsx
+++ src/app/components/NavMenu.tsx
@@ -10,13 +10,18 @@ type Props = {
 
 export default function NavMenu({ navMenuList }: Props) {
     const pathname = usePathname();
+
+    const hasPathname = (path: string): boolean => {
+        return pathname.split("/")[1] === path.split("/")[1];
+    };
+
     return (
         <>
             {navMenuList.map(({ title, path }) => (
                 <li key={title} className="text-lg text-center">
                     <Link
                         href={path}
-                        className={`${pathname === path && "text-blue-600 dark:text-yellow-500"}`}
+                        className={`${hasPathname(path) && "text-blue-600 dark:text-yellow-500"}`}
                     >
                         {title}
                     </Link>
