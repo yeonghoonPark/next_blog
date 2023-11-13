676c116d5ddd655808b4d1644229c5604a00584f
yeonghoonPark <huklmnj2@naver.com>
2023-11-13 23:43:56 +0900

    [feature] PostsPage, working on CSS for PC screen

--- src/app/(route)/page.tsx
+++ src/app/(route)/page.tsx
@@ -1,35 +1,20 @@
 import { allPosts } from "@/contentlayer/generated";
-import { compareDesc } from "date-fns";
 import PostsGrid from "../components/PostsGrid";
 
 const FEATURED_POSTS_TITLE = "FEATURED POSTS";
-const ALL_POSTS_TITLE = "ALL POSTS";
 
 export default function HomePage() {
     const featuredPosts = allPosts.filter(({ featured }) => featured);
     const featuredPostsCount = featuredPosts.length;
-    const sortedAllPosts = allPosts.sort((a, b) =>
-        compareDesc(new Date(a.createdAt), new Date(b.createdAt))
-    );
-    const allPostsCount = allPosts.length;
 
     return (
-        <>
-            <section className="mb-12">
-                <h2 className="text-xl font-semibold my-4">
-                    {FEATURED_POSTS_TITLE}&nbsp;
-                    <span className="text-emerald-500">&nbsp;{`${featuredPostsCount}`}</span>
-                </h2>
-                <PostsGrid posts={featuredPosts} />
-            </section>
-
-            <section className="mb-12">
-                <h2 className="text-xl font-semibold my-4">
-                    {ALL_POSTS_TITLE}&nbsp;
-                    <span className="text-emerald-500">&nbsp;{`${allPostsCount}`}</span>
-                </h2>
-                <PostsGrid max={4} posts={sortedAllPosts} />
-            </section>
-        </>
+        <section className="mb-12">
+            {/* ##$$ Todo, SectionTitle 컴포넌트화 */}
+            <h2 className="text-xl font-semibold my-4">
+                {FEATURED_POSTS_TITLE}&nbsp;
+                <span className="text-emerald-500">&nbsp;{`${featuredPostsCount}`}</span>
+            </h2>
+            <PostsGrid posts={featuredPosts} />
+        </section>
     );
 }
--- src/app/(route)/posts/page.tsx
+++ src/app/(route)/posts/page.tsx
@@ -1,7 +1,39 @@
+import PostsGrid from "@/app/components/PostsGrid";
+import { allPosts } from "@/contentlayer/generated";
+import { compareDesc } from "date-fns";
+
+const ALL_POSTS_TITLE = "ALL POSTS";
+const CATEGORY_TITLE = "CATEGORY";
+const categories = ["All", "JavaScript", "TypeScript", "React.js", "NEXT.js", "etc"];
+
 export default function PostsPage() {
+    const sortedAllPosts = allPosts.sort((a, b) =>
+        compareDesc(new Date(a.createdAt), new Date(b.createdAt))
+    );
+    const allPostsCount = allPosts.length;
+
     return (
-        <>
-            <h1>PostsPage</h1>
-        </>
+        <section className="flex flex-col-reverse sm:flex-col-reverse md:flex-row">
+            <section className="mb-12">
+                <h2 className="text-xl font-semibold my-4">
+                    {ALL_POSTS_TITLE}&nbsp;
+                    <span className="text-emerald-500">&nbsp;{`${allPostsCount}`}</span>
+                </h2>
+                <PostsGrid posts={sortedAllPosts} />
+            </section>
+
+            <div className="w-1 m-4" />
+
+            <section className="flex flex-col items-center py-1">
+                <h2 className="w-fit text-lg font-semibold border-b border-emerald-500 my-4">
+                    {CATEGORY_TITLE}
+                </h2>
+                <ul className="flex flex-col items-center gap-3">
+                    {categories.map((category, i) => (
+                        <li key={i}>{category}</li>
+                    ))}
+                </ul>
+            </section>
+        </section>
     );
 }
