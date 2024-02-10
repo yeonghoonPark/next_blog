8a64720d93c89297994162a7e78b29160bfee379
yeonghoonPark <huklmnj2@naver.com>
2024-02-10 19:01:52 +0900

    [components] PostsGrid.tsx, edit css
    detail note :
    
    1. edit gap

--- src/app/components/PostsGrid.tsx
+++ src/app/components/PostsGrid.tsx
@@ -1,30 +1,35 @@
-import { memo } from "react";
+import { memo } from 'react';
 
-import Link from "next/link";
+import Link from 'next/link';
 
-import { Post } from "@/contentlayer/generated";
+import { Post } from '@/contentlayer/generated';
 
-import PostCard from "./PostCard";
+import PostCard from './PostCard';
 
 type Props = {
-    posts: Post[];
+  posts: Post[];
 };
 
 function PostsGrid({ posts }: Props) {
-    return (
-        <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-x-6 gap-y-12">
-            {posts.map((post) => (
-                <li
-                    key={post.title}
-                    className="rounded-md rounded-ss-3xl rounded-ee-3xl overflow-hidden border border-neutral-300 dark:border-gray-700 group"
-                >
-                    <Link href={`/posts/${post._raw.flattenedPath}`}>
-                        <PostCard post={post} />
-                    </Link>
-                </li>
-            ))}
-        </ul>
-    );
+  return (
+    <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+      {posts.map((post) => (
+        <li
+          key={post.title}
+          className="
+            overflow-hidden 
+            border border-neutral-300 dark:border-gray-700 
+            rounded-md rounded-ss-3xl rounded-ee-3xl 
+            group
+          "
+        >
+          <Link href={`/posts/${post._raw.flattenedPath}`}>
+            <PostCard post={post} />
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export default memo(PostsGrid);
