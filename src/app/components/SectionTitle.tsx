8d69bb641bbd988a58c4d9466b860bb5cb1fe1e5
yeonghoonPark <huklmnj2@naver.com>
2024-02-10 18:50:34 +0900

    [components] SectionTitle.tsx, edit css
    detail note :
    
    1. delete &nbsp;
    2. edit display

--- src/app/components/SectionTitle.tsx
+++ src/app/components/SectionTitle.tsx
@@ -1,17 +1,17 @@
-import { memo } from "react";
+import { memo } from 'react';
 
 type Props = {
-    title: string;
-    count: string | number;
+  title: string;
+  count: string | number;
 };
 
 function SectionTitle({ title, count }: Props) {
-    return (
-        <h2 className="text-xl font-bold my-4">
-            {title}&nbsp;
-            <span className="text-rose-500 dark:text-pink-400">&nbsp;{`${count}`}</span>
-        </h2>
-    );
+  return (
+    <h2 className="flex items-center gap-2 my-4 font-bold text-xl">
+      {title}
+      <span className="text-rose-500 dark:text-pink-400">{count}</span>
+    </h2>
+  );
 }
 
 export default memo(SectionTitle);
