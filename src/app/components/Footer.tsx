5dfa3544c6ee1df9fa3d4dbfe6d94cad82ddf79e
yeonghoonPark <huklmnj2@naver.com>
2024-08-24 20:22:13 +0900

    [components] "Edit, change text '2023' to '2024' in Footer.jsx"
    Changed files:
    1. Footer.jsx

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,9 +1,9 @@
-const FOOTER_CONTENTS = "© 2023 • Yeong Chi";
+const FOOTER_CONTENTS = "© 2024 • Yeong Chi";
 
 export default function Footer() {
-    return (
-        <footer className="flex justify-center self-center w-full py-3 border-t-[0.2px] border-neutral-300 dark:border-gray-700">
-            <p className="text-sm font-light text-gray-400">{FOOTER_CONTENTS}</p>
-        </footer>
-    );
+  return (
+    <footer className="flex justify-center self-center w-full py-3 border-t-[0.2px] border-neutral-300 dark:border-gray-700">
+      <p className="text-sm font-light text-gray-400">{FOOTER_CONTENTS}</p>
+    </footer>
+  );
 }
