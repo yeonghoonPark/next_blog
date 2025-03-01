d7efbf6bc7e54e564e69dab6cf5cb8c9902eead8
yeonghoonPark <huklmnj2@naver.com>
2025-03-02 00:52:01 +0900

    feature: add "useCategoryStore" for navigation

---
+++ src/app/store/useCategoryStore.ts
@@ -0,0 +1,23 @@
+import { create } from "zustand";
+import { Category } from "@/app/models/posts";
+
+type CategoryActions = {
+  resetCategory: () => void;
+  setCategory: (category: Category) => void;
+};
+type CategoryStore = {
+  category: Category;
+  actions: CategoryActions;
+};
+
+const initialState = {
+  category: "All" as Category,
+};
+
+export const useCategoryStore = create<CategoryStore>((set) => ({
+  ...initialState,
+  actions: {
+    resetCategory: () => set(initialState),
+    setCategory: (category) => set(() => ({ category })),
+  },
+}));
