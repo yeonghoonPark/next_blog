782969f55f277185ca5773a0990a34943aacad1d
yeonghoonPark <huklmnj2@naver.com>
2025-03-03 23:17:11 +0900

    chore: modify constant variable CATEGORIES

--- src/app/constants/posts/index.ts
+++ src/app/constants/posts/index.ts
@@ -1,6 +1,8 @@
 import { Category } from "@/app/models/posts";
 
+// TypeScript, NEXT.js
+
 export const ALL = "All";
-export const CATEGORIES: Category[] = ["All", "JavaScript", "TypeScript", "React.js", "NEXT.js"];
+export const CATEGORIES: Category[] = ["All", "JavaScript", "React.js"];
 export const CATEGORY_TITLE = "Category";
 export const POSTS_TITLE = "Posts";
