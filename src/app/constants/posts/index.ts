5eeb6ca0537b74ec171ee1664a0ce82d92a0f866
yeonghoonPark <huklmnj2@naver.com>
2025-03-05 01:27:11 +0900

    chore: modify constant for posts
    add
    CATEGORY_ALL
    CATEGORY_JAVASCRIPT
    CATEGORY_NEXTJS
    CATEGORY_REACT
    CATEGORY_TYPESCRIPT

--- src/app/constants/posts/index.ts
+++ src/app/constants/posts/index.ts
@@ -1,8 +1,10 @@
 import { Category } from "@/app/models/posts";
 
-// TypeScript, NEXT.js
-
-export const ALL = "All";
 export const CATEGORIES: Category[] = ["All", "JavaScript", "React.js"];
+export const CATEGORY_ALL = "All";
+export const CATEGORY_JAVASCRIPT = "JavaScript";
+export const CATEGORY_NEXTJS = "NEXT.js";
+export const CATEGORY_REACT = "React.js";
+export const CATEGORY_TYPESCRIPT = "TypeScript";
 export const CATEGORY_TITLE = "Category";
 export const POSTS_TITLE = "Posts";
