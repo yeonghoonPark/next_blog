e41f14e06bd146d7967f311fdd34d1491e915579
yeonghoonPark <huklmnj2@naver.com>
2025-03-06 10:51:49 +0900

    style: change Footer component to be hidden

--- src/app/components/organisms/Footer.tsx
+++ src/app/components/organisms/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="flex justify-center py-3 border-neutral-300 dark:border-gray-700 border-t-[0.2px] w-full">
+    <footer className="hidden justify-center py-3 border-neutral-300 dark:border-gray-700 border-t-[0.2px] w-full">
       <p className="font-light text-gray-400 text-sm">{`© ${currentYear} • PPOTTA`}</p>
     </footer>
   );
