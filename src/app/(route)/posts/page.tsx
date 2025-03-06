e05d32e143e659321022ad61cf1f44cceac5afc4
yeonghoonPark <huklmnj2@naver.com>
2025-03-06 22:16:32 +0900

    🧹 chore: remove unnecessary constant variable

--- src/app/(route)/posts/page.tsx
+++ src/app/(route)/posts/page.tsx
@@ -3,7 +3,7 @@
 import { compareDesc } from "date-fns";
 import SectionTitle from "@/app/components/molecules/SectionTitle";
 import PostsGrid from "@/app/components/organisms/PostsGrid";
-import { CATEGORY_ALL, POSTS_TITLE } from "@/app/constants/posts";
+import { CATEGORY_ALL, POSTS } from "@/app/constants/posts";
 import { Category } from "@/app/models/posts";
 import { useCategoryStore } from "@/app/store/posts/useCategoryStore";
 import { allPosts } from "@/contentlayer/generated";
@@ -11,14 +11,12 @@ import { allPosts } from "@/contentlayer/generated";
 const PostsPage = () => {
   const category = useCategoryStore((state) => state.category);
 
-  const sortedAllPosts = allPosts.sort((a, b) =>
-    compareDesc(new Date(a.createdAt), new Date(b.createdAt)),
-  );
-  const filteredPosts = sortedAllPosts.filter((post) =>
-    category === CATEGORY_ALL ? post : post.category === category,
-  );
+  const sortedAllPosts = //
+    allPosts.sort((a, b) => compareDesc(new Date(a.createdAt), new Date(b.createdAt)));
+  const filteredPosts = //
+    sortedAllPosts.filter((post) => post.category === category || category === CATEGORY_ALL);
 
-  const getTitle = (category: Category) => `${category} ${POSTS_TITLE}`;
+  const getTitle = (category: Category) => `${category} ${POSTS}`;
 
   return (
     <section>
--- src/app/constants/posts/index.ts
+++ src/app/constants/posts/index.ts
@@ -7,5 +7,4 @@ export const CATEGORY_JAVASCRIPT = "JavaScript";
 export const CATEGORY_NEXTJS = "NEXT.js";
 export const CATEGORY_REACT = "React";
 export const CATEGORY_TYPESCRIPT = "TypeScript";
-export const CATEGORY_TITLE = "Category";
-export const POSTS_TITLE = "Posts";
+export const POSTS = "Posts";
