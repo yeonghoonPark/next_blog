0494129e3cd3a9bbfeaeb46c3f6e761cb30c9272
yeonghoonPark <huklmnj2@naver.com>
2025-07-06 21:17:57 +0900

    💫 feat: set contentlayer config for the reflection parts

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -3,49 +3,77 @@ import rehypePrettyCode from "rehype-pretty-code";
 import rehypeSlug from "rehype-slug";
 
 export const Post = defineDocumentType(() => ({
-    name: "Post",
-    contentType: "mdx",
-    filePathPattern: "**/*.mdx",
-    fields: {
-        title: {
-            type: "string",
-            required: true,
-        },
-        description: {
-            type: "string",
-            required: true,
-        },
-        category: {
-            type: "string",
-            required: true,
-        },
-        thumbnail: {
-            type: "string",
-            required: false,
-        },
-        createdAt: {
-            type: "date",
-            required: true,
-        },
-        featured: {
-            type: "boolean",
-            required: true,
-        },
+  name: "Post",
+  contentType: "mdx",
+  filePathPattern: "posts/**/*.mdx",
+  fields: {
+    category: {
+      type: "string",
+      required: true,
     },
+    createdAt: {
+      type: "date",
+      required: true,
+    },
+    description: {
+      type: "string",
+      required: true,
+    },
+    featured: {
+      type: "boolean",
+      required: true,
+    },
+    thumbnail: {
+      type: "string",
+      required: false,
+    },
+    title: {
+      type: "string",
+      required: true,
+    },
+  },
+}));
+
+export const Reflection = defineDocumentType(() => ({
+  name: "Reflection",
+  contentType: "mdx",
+  filePathPattern: "reflections/**/*.mdx",
+  fields: {
+    category: {
+      type: "string",
+      required: true,
+    },
+    createdAt: {
+      type: "date",
+      required: true,
+    },
+    description: {
+      type: "string",
+      required: true,
+    },
+    thumbnail: {
+      type: "string",
+      required: false,
+    },
+    title: {
+      type: "string",
+      required: true,
+    },
+  },
 }));
 
 const rehypeOptions = {
-    theme: "dark-plus",
-    keepBackground: false,
+  theme: "dark-plus",
+  keepBackground: false,
 };
 
 const contentSource = makeSource({
-    contentDirPath: "data/posts",
-    documentTypes: [Post],
-    mdx: {
-        remarkPlugins: [],
-        rehypePlugins: [[rehypePrettyCode, rehypeOptions], rehypeSlug],
-    },
+  contentDirPath: "data",
+  documentTypes: [Post, Reflection],
+  mdx: {
+    remarkPlugins: [],
+    rehypePlugins: [[rehypePrettyCode, rehypeOptions], rehypeSlug],
+  },
 });
 
 export default contentSource;
